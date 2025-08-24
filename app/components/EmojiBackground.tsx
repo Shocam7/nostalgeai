6d3c2b24d37fb2979987c7f81f0b76398fad72c9
Shocam7 <87086200+Shocam7@users.noreply.github.com>
2025-08-24 14:30:02 +0530

    Update EmojiBackground.tsx

--- app/components/EmojiBackground.tsx
+++ app/components/EmojiBackground.tsx
@@ -11,7 +11,6 @@ function generateEmojiArray(): string[] {
     [0x1F600, 0x1F64F], // Emoticons
     [0x1F300, 0x1F5FF], // Misc Symbols and Pictographs
     [0x1F680, 0x1F6FF], // Transport and Map
-    [0x1F1E0, 0x1F1FF], // Regional indicator symbols (flags)
     [0x2600, 0x26FF],   // Misc symbols
     [0x2700, 0x27BF],   // Dingbats
     [0x1F900, 0x1F9FF], // Supplemental Symbols and Pictographs
