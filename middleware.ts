2fa8fc00a176c4767e4a21f70b65e53f066a625f
Shocam7 <87086200+Shocam7@users.noreply.github.com>
2025-11-18 08:58:24 +0530

    Add city and region headers to middleware response

--- middleware.ts
+++ middleware.ts
@@ -2,9 +2,13 @@ import { NextResponse } from "next/server";
 
 export function middleware(req: Request) {
   const country = req.headers.get("x-vercel-ip-country") || "UNKNOWN";
+  const city = req.headers.get("x-vercel-ip-city") || "";
+  const region = req.headers.get("x-vercel-ip-region") || "";
 
   const res = NextResponse.next();
   res.headers.set("x-user-country", country);
+  res.headers.set("x-user-city", city);
+  res.headers.set("x-user-region", region);
 
   return res;
 }
